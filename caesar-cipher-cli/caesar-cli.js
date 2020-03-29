b28c822f319b39bce15deff1769311b62e644a36
vladimir-drachuk <vvdra4@gmail.com>
2020-03-29 18:46:12 +0300

    refactor: delete Readable stream

--- caesar-cipher-cli/caesar-cli.js
+++ caesar-cipher-cli/caesar-cli.js
@@ -1,26 +1,26 @@
-'use strict';
-
 const { program } = require('commander');
-const { encryption } = require('./encryptor')
+const { encryption } = require('./encryptor');
 
 program
-    .requiredOption('-s, --shift <number>', 'a shift')
-    .requiredOption('-a, --action <action>', 'an action encode/decode')
-    .option('-i, --input <file-path>', 'an input file')
-    .option('-o, --output <file-path>', 'an output file')
-    .action(() => {
-        const keys = program.opts();
-        switch (keys.action) {
-            case 'encode':
-                encryption(keys);
-            break;
-            case 'decode':
-                keys.shift *= -1;
-                encryption(keys);
-            break
-            default:
-            console.error(`error:  required option '-a, --action <action>' not specified or incorrect action enter(it can be only encode or decode)`);
-        }
-    });
+  .requiredOption('-s, --shift <number>', 'a shift')
+  .requiredOption('-a, --action <action>', 'an action encode/decode')
+  .option('-i, --input <file-path>', 'an input file')
+  .option('-o, --output <file-path>', 'an output file')
+  .action(() => {
+    const keys = program.opts();
+    switch (keys.action) {
+      case 'encode':
+        encryption(keys);
+        break;
+      case 'decode':
+        keys.shift *= -1;
+        encryption(keys);
+        break;
+      default:
+        console.error(
+          "error:  required option '-a, --action <action>' not specified or incorrect action enter(it can be only encode or decode)"
+        );
+    }
+  });
 
 program.parse(process.argv);
--- caesar-cipher-cli/encryptor.js
+++ caesar-cipher-cli/encryptor.js
@@ -1,27 +1,19 @@
-'use strict';
-
 const fs = require('fs');
-const { pipeline, Readable } = require('stream');
-const CaesarCipher = require('./transform-stream')
+const { pipeline } = require('stream');
+const CaesarCipher = require('./transform-stream');
 
 function encryption(keys) {
-    const transformStream = new CaesarCipher(keys.shift);
-    let readFormStd;
-
-    if (!keys.input) {
-        readFormStd = Readable.from(process.stdin);
+  const transformStream = new CaesarCipher(keys.shift);
+  pipeline(
+    keys.input ? fs.createReadStream(keys.input) : process.stdin,
+    transformStream,
+    keys.output
+      ? fs.createWriteStream(keys.output, { flags: 'a' })
+      : process.stdout,
+    err => {
+      if (err) console.error('Pipline failed', err);
     }
-    
-    pipeline(
-        (keys.input)
-            ? fs.createReadStream(keys.input)
-            : readFormStd,
-        transformStream,
-        (keys.output)
-            ? fs.createWriteStream(keys.output, { flags: 'a' })
-            : process.stdout,
-        (err) => { if (err) console.error('Pipline failed', err); },
-    )
+  );
 }
 
 module.exports = { encryption };
