6ff8c1385ce3a3024c72a45e7e512b1242f17ceb
Vladimir Drachuk <vvdra4@gmail.com>
2020-04-06 16:09:47 +0300

    refactor: delete my own users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,27 +1,4 @@
-const User = require('./user.model');
-
 const users = [];
-
-const petya = new User({
-  name: 'PETYA',
-  login: 'petya1993',
-  password: 'P@55w0rd1'
-});
-users.push(petya);
-
-const vova = new User({
-  name: 'VOVA',
-  login: 'vova1987',
-  password: 'P@55w0rd2'
-});
-users.push(vova);
-
-const masha = new User({
-  name: 'MASHA',
-  login: 'masha_beauty',
-  password: 'P@55w0rd3'
-});
-users.push(masha);
 
 const getAll = async () => {
   return users;
