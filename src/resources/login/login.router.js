0454bce0380979321b401e53a1fa0b1cb79f4a63
vladimir-drachuk <vvdra4@gmail.com>
2020-04-26 16:31:28 +0300

    feat: add forbidden error

--- src/resources/login/login.router.js
+++ src/resources/login/login.router.js
@@ -1,20 +1,24 @@
 const router = require('express').Router();
 const { errorHandler } = require('../../common/error-handler');
 const loginService = require('./login.service');
-const { LOCKED } = require('http-status-codes');
+const { FORBIDDEN, LOCKED } = require('http-status-codes');
 const userService = require('../users/user.service');
 
 router.route('/').post(async (req, res) => {
   const user = await userService.getUserByLogin(req.body.login);
-  const isValid = await loginService.isPasswordValid(
-    user.password,
-    req.body.password
-  );
-  if (isValid) {
-    const token = loginService.getToken(user._id, user.login);
-    res.header('Authorization', token).send({ token });
+  if (user) {
+    const isValid = await loginService.isPasswordValid(
+      user.password,
+      req.body.password
+    );
+    if (isValid) {
+      const token = loginService.getToken(user._id, user.login);
+      res.header('Authorization', token).send({ token });
+    } else {
+      errorHandler(res, LOCKED, 'incorrect password');
+    }
   } else {
-    errorHandler(res, LOCKED, 'User not autorised');
+    errorHandler(res, FORBIDDEN, 'user not authorized');
   }
 });
 
